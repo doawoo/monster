f66f350726e7063de52afec8f0e07f33eeaf0f8a
Digit <woof@puppy.surf>
2020-10-28 21:09:15 -0700

    remove useless code

--- lib/monster_web/plugs/token_auth_plug.ex
+++ lib/monster_web/plugs/token_auth_plug.ex
@@ -10,8 +10,6 @@ defmodule MonsterWeb.Plugs.TokenAuthPlug do
     with [token_string] <- Plug.Conn.get_req_header(conn, "token"),
     {:ok, token_data} <- Phoenix.Token.verify(MonsterWeb.Endpoint, @token_salt, token_string),
     :gt <- NaiveDateTime.compare(token_data.expires, NaiveDateTime.utc_now) do
-      # validate token expiration
-      NaiveDateTime.utc_now() |> NaiveDateTime.compare(token_data.expires)
       conn |> assign(:token, token_data)
     else
       _ -> conn |> resp(:unauthorized, "unauthorized") |> halt()
